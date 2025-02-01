dd3d404ae4959e83e3499ff5eefc5f89f3362a56
Ondra Janek <ondra.janek1@gmail.com>
2025-02-01 18:32:45 +0100

    Remove "core" application

--- app/urls.py
+++ app/urls.py
@@ -21,5 +21,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls", namespace="accounts")),
-    path("/", include("core.urls", namespace="core")),
 ]
