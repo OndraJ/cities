7c876bd973e1437a1b972c936fc1ca87e38ac075
Ondra Janek <ondra.janek1@gmail.com>
2025-02-09 16:33:46 +0100

    Implement password reset functionality for registered users

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,38 @@
 from django.urls import path
-
-from accounts.views import UserRegistrationView, UserLoginView, EmailConfirmationView
+from accounts.views import (
+    UserRegistrationView,
+    UserLoginView,
+    EmailConfirmationView,
+    CustomPasswordResetView,
+    CustomPasswordResetConfirmView,
+    CustomPasswordResetCompleteView,
+    CustomPasswordResetDoneView,
+)
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("", UserLoginView.as_view(), name="login"),
+    path("login/", UserLoginView.as_view(), name="login"),
     path("register/", UserRegistrationView.as_view(), name="register"),
     path(
         "email_confirm/<uidb64>/<token>/",
         EmailConfirmationView.as_view(),
         name="email_confirm",
+    ),
+    path("reset-password/", CustomPasswordResetView.as_view(), name="password_reset"),
+    path(
+        "reset-password/done/",
+        CustomPasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset-password/confirm/<uidb64>/<token>/",
+        CustomPasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset-password/complete/",
+        CustomPasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
     ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,17 @@
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm, SetPasswordForm
+from django.contrib.auth.views import (
+    LoginView,
+    PasswordResetView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+    PasswordResetDoneView,
+)
 from django.shortcuts import redirect
 from django.urls import reverse_lazy, reverse
 from django.utils.http import urlsafe_base64_decode
 from django.views import View
 from django.views.generic import FormView
-
 from accounts.forms import CustomUserCreationForm
 from accounts.helpers import TokenGenerator, confirm_email
 from accounts.models import User
@@ -23,8 +28,8 @@ class UserRegistrationView(FormView):
         user.save()
         messages.success(
             self.request,
-            f"Dear {form.cleaned_data.get("username")}, \n"
-            f"Please confirm your email address: {form.cleaned_data.get("email")} to activate your account. \n",
+            f"Dear {form.cleaned_data.get('username')}, \n"
+            f"Please confirm your email address: {form.cleaned_data.get('email')} to activate your account. \n",
         )
         confirm_email(self.request, user)
         return super().form_valid(form)
@@ -60,3 +65,33 @@ class EmailConfirmationView(View):
                 self.request, "The confirmation link is invalid or has expired."
             )
             return redirect(reverse("accounts:register"))
+
+
+class CustomSetPasswordForm(SetPasswordForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.help_text = None
+
+
+class CustomPasswordResetView(PasswordResetView):
+    template_name = "accounts/password_reset_form.html"
+    email_template_name = "accounts/password_reset_email.txt"
+    html_email_template_name = "accounts/password_reset_email.txt"
+    success_url = reverse_lazy("accounts:password_reset_done")
+
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    form_class = CustomSetPasswordForm
+    template_name = "accounts/password_reset_confirm.html"
+    success_url = reverse_lazy("accounts:password_reset_complete")
+
+
+class CustomPasswordResetCompleteView(PasswordResetCompleteView):
+    template_name = "accounts/password_reset_complete.html"
+    success_url = reverse_lazy("accounts:login")
+
+
+class CustomPasswordResetDoneView(PasswordResetDoneView):
+    template_name = "accounts/password_reset_done.html"
